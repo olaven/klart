ffa08b04f32813e82a01687ef126195e1669aedc
olaven <olav@sundfoer.com>
2020-08-20 19:35:45 +0000

    Improved test coverage

--- src/klar.ts
+++ src/klar.ts
@@ -3,17 +3,12 @@ import { WithDatabase } from './WithDatabase'
 import { queries } from './queries'
 
 export const klar = (config: PoolConfig = {}) => {
-  const pool = new Pool(config)
+    const pool = new Pool(config)
 
-  pool.on('error', (err, client) => {
-    console.error('Unexpected error on idle client', err)
-    process.exit(-1)
-  })
+    const withDatabase: WithDatabase = <T>(action: (pool: Pool) => Promise<T>) => action(pool)
 
-  const withDatabase: WithDatabase = <T>(action: (pool: Pool) => Promise<T>) => action(pool)
-
-  return {
-    ...queries(withDatabase),
-    end: pool.end
-  }
+    return {
+        ...queries(withDatabase),
+        end: pool.end
+    }
 }
--- tests/klar.test.ts
+++ tests/klar.test.ts
@@ -25,37 +25,29 @@ describe('Klar', () => {
   });
 
   afterAll(async () => {
-    await end()
+    //await end();
   });
 
   describe('Klar in general', () => {
+
     it('Does not crash on load', () => {
       expect(first).toBeDefined()
       expect(rows).toBeDefined()
       expect(run).toBeDefined()
     });
 
-    it('Does accept a PoolConfig object', () => {
-      expect(async () => {
-        const k = klar({
-          port: 1234,
-          user: 'some test user hello'
-        });
-
-        await k.end();
-      }).not.toThrow()
-    });
     it('Appears to pick up values from env', async () => {
+
       expect(process.env.PGHOST).toBeDefined()
       expect(process.env.PGUSER).toBeDefined()
       expect(process.env.PGPASSWORD).toBeDefined()
       expect(process.env.PGDATABASE).toBeDefined()
 
-      const result = await first(
-        `select max(table_catalog) as x from information_schema.tables`,
-        []
-      )
-      expect(result).toBeDefined()
+      const databaseResult = await first<any>(`select current_database()`);
+      const userResult = await first<any>(`select current_user`);
+
+      expect(databaseResult.current_database).toEqual(process.env.PGDATABASE);
+      expect(userResult.current_user).toEqual(process.env.PGUSER);
     });
 
     describe('Getting first row', () => {
@@ -70,7 +62,6 @@ describe('Klar', () => {
 
         expect(dog).not.toBeNull()
 
-        console.log(dog)
         expect(dog?.name).toEqual(name)
         expect(dog?.age).toEqual(age)
       });
@@ -100,6 +91,25 @@ describe('Klar', () => {
         for (const id of ids) {
           expect(all.includes(id)).toBeTruthy()
         }
+      });
+
+      it('`first` returns null if no first row is returned', async () => {
+
+        const result = await first('select * from dogs where id = $1', [faker.random.number({ min: 30000, max: 100000 })]);
+        expect(result).toBeNull();
+      });
+
+      it('is possible to pass `values` to `rows`', async () => {
+
+        const newName = faker.name.firstName();
+
+        const original = await first<Dog>(`INSERT INTO dogs (name, age) VALUES($1, $2) RETURNING *`, [faker.name.firstName(), faker.random.number()]);
+        await rows(`update dogs set name = $1 where id = $2`, [newName, original.id]);
+        const updated = await first<Dog>('select * from dogs where id = $1', [original.id]);
+
+        expect(original.id).toEqual(updated.id);
+        expect(original.name).not.toEqual(newName);
+        expect(updated.name).toEqual(newName);
       });
     });
   });
